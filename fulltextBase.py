92cbe0be198eddaf0c9335a952d0d96386edeb02
noname30081 <noname30081@gmail.com>
2021-10-04 22:58:46 +0800

    test_publish

--- fulltextBase.py
+++ fulltextBase.py
@@ -7,23 +7,21 @@ from keywordBase import KeyWordBase
 
 
 url = input('Give The URL : ')
-
-print(KeyWordBase.lis[0])
 driver = webdriver.Chrome()
 #driver.get("file:///D:/Master%20Degree/Lesson/web%20crawler/jof/index.html?format=json")
 driver.get(url)
 
-# 定位搜尋框
 try:
+    #Wait Web Open
     WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "about"))
             )
+    #Get Spefic Element
     element = driver.find_element_by_class_name("full.margin_top_20")
-    #element = WebDriverWait(driver, 10).until(driver.find_element_by_class_name("full margin_top_20"))
+    #Split Sentences
     list = KeyWordBase.SplitSentences(element.text)
-    #list = element.text.split("[.+ +A-Z]")
+    #Choose Sentences By Keyword
     list = KeyWordBase.KeywordSectence(list,"tempor")
-    print(list[0]) 
 except Exception as e:
     print(e)
     quit();
