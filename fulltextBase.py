1a84dfe4fc1ef866b60a4a02bbcf8b1e7b4e4222
noname30081 <noname30081@gmail.com>
2021-10-04 23:36:40 +0800

    Create FullTextBass Method

--- fulltextBase.py
+++ fulltextBase.py
@@ -1,31 +1,72 @@
+from enum import Enum
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from keywordBase import KeyWordBase
 
 
-url = input('Give The URL : ')
-driver = webdriver.Chrome()
-#driver.get("file:///D:/Master%20Degree/Lesson/web%20crawler/jof/index.html?format=json")
-driver.get(url)
+class DrvBase(Enum) :
+    Chrome = 0
+    FireFox = 1
+    Edge = 2
+    _IE = 3
+    Opera = 4
 
-try:
+class FullTextBase : 
+    def CreatePage(url,drvbase : DrvBase) :
+        #Get Driver
+        if(drvbase == DrvBase.Chrome) :
+            driver = webdriver.Chrome()
+        elif(drvbase == DrvBase.FireFox) :
+            driver = webdriver.Firefox()
+        elif(drvbase == DrvBase.Edge) :
+            driver = webdriver.Edge()
+        elif(drvbase == DrvBase._IE) :
+            driver = webdriver.Ie()     
+        else :
+            driver = webdriver.Opera()     
+        #Connect Url
+        driver.get(url)
+        return driver , url
+    
+    def GetSentencesByClassName(driver,classname,keyword):
+        #Get Spefic Element
+        element = driver.find_element_by_class_name(classname)
+        #Split Sentences
+        Sents = KeyWordBase.SplitSentences(element.text)
+        #Choose Sentences By Keyword
+        keySents = KeyWordBase.KeywordSectence(Sents,keyword)
+        return Sents,keySents
+
+        
+
+
+
+
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.by import By
+
+
+#url = input('Give The URL : ')
+#driver = webdriver.Chrome()
+#driver.get("file:///D:/Master%20Degree/Lesson/web%20crawler/jof/index.html?format=json")
+#driver.get(url)
+
+#try:
     #Wait Web Open
-    WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, "about"))
-            )
+    #WebDriverWait(driver, 20).until(
+    #        EC.presence_of_element_located((By.ID, "about"))
+    #        )
     #Get Spefic Element
-    element = driver.find_element_by_class_name("full.margin_top_20")
+    #element = driver.find_element_by_class_name("full.margin_top_20")
     #Split Sentences
-    list = KeyWordBase.SplitSentences(element.text)
+    #list = KeyWordBase.SplitSentences(element.text)
     #Choose Sentences By Keyword
-    list = KeyWordBase.KeywordSectence(list,"tempor")
-except Exception as e:
-    print(e)
-    quit();
-driver.quit()
+    #list = KeyWordBase.KeywordSectence(list,"tempor")
+#except Exception as e:
+    #print(e)
+    #quit();
+#driver.quit()
 
 
 # 傳入字串
