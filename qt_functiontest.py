d03b9ac631aeed3f3242ae1a0b7a903933eae370
noname30081 <noname30081@gmail.com>
2021-10-16 21:51:31 +0800

    homework1 Addition - qt form tab 'HW1 - Addition'

--- qt_functiontest.py
+++ qt_functiontest.py
@@ -9,7 +9,7 @@ import gui.Ui_SearchQt as ui
 from SearchEngine import SearchEngine as engine
 from SearchEngine import LoadMode
 from fulltextBase import Format
-from SearchEngine import SearchEngine
+from SearchEngine import SearchEngine 
 
 class Main(QMainWindow, ui.Ui_MainWindow):
     bb = ''
@@ -22,6 +22,10 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         self.btn_SetRoot.clicked.connect(self.btn_SetRoot_Click)
         self.TV_Root.clicked.connect(self.TV_Root_Click)
         self.btn_SearchKey.clicked.connect(self.btn_search_click)
+        self.rbt_ATC_Cheat.clicked.connect(self.rbt_ATC_Cheat_click)
+        self.rbt_ATC_CharCheck.clicked.connect(self.rbt_ATC_CharCheck_click)
+        self.rbt_ATC_WardCheck.clicked.connect(self.rbt_ATC_WordCheck_click)
+    #region <WH1>
     def btn_SetRoot_Click(self) :
         directory = QtWidgets.QFileDialog.getExistingDirectory(self,"Video Directory", QDir.currentPath());
         self.txt_Root.setText(directory)
@@ -60,6 +64,7 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         self.textEdit.setHtml(strColor)
         self.textEdit.setStyleSheet("background-color:black")
         self.lbl_MSens.setText(str(len(keysents)))
+    #endregion
     def SentencesBrKeyWords(self, sent , keywords ) :
         strOut = ''
         for sen in sent :
@@ -67,6 +72,15 @@ class Main(QMainWindow, ui.Ui_MainWindow):
                 sen = sen.replace(kw,'<span style="background-color:white;color:black">{}</span>'.format(kw))
             strOut+='{}<br><br>'.format(sen)
         return strOut
+    def rbt_ATC_Cheat_click(self) :
+        self.gb_ATC_charMode.setEnabled(False)
+        self.gb_ATC_wordMode.setEnabled(False)
+    def rbt_ATC_CharCheck_click(self) :
+        self.gb_ATC_charMode.setEnabled(True)
+        self.gb_ATC_wordMode.setEnabled(False)   
+    def rbt_ATC_WordCheck_click(self) :
+        self.gb_ATC_charMode.setEnabled(False)
+        self.gb_ATC_wordMode.setEnabled(True)                
 
 
 if __name__ == '__main__':
